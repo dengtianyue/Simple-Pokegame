77b77c1dc0af04e9c32dde64bb763b0ee6dc2c1c
Skycheer <15605825025@163.com>
2016-07-26 23:34:00 +0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,24 @@
+package Pokegame;
+/**
+ * 定义类 Card及其set，get方法
+ * @author tyt
+ *
+ */
+public class Card {
+	String cardShape;
+	String cardId;
+	public String getCardShape() {
+		return cardShape;
+	}
+	public void setCardShape(String cardShape) {
+		this.cardShape = cardShape;
+	}
+	public String getCardId() {
+		return cardId;
+	}
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	
+
+}
